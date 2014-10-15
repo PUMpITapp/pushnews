6b8369f16c0196bdf35a1cfc7bff9e73319847b8
Pierre Leca <piele739@student.liu.se>
2014-10-15 19:14:33 +0200

    feed getter and parser, installation of feedparser and lua-socket required

---
+++ src/feeds/feeds.lua
@@ -0,0 +1,52 @@
+local http = require("socket.http")
+local io = require("io")
+local ltn12 = require("ltn12")
+require "feedparser"
+
+FeedGetter = {}
+FeedGetter.__index = FeedsGetter
+
+function FeedGetter:new()
+	newObj = {
+		
+	}
+  self.__index = self
+  return setmetatable(newObj, self)
+end
+
+function FeedGetter:downloadFeeds(url, filename)
+	print(filename)
+
+	outputfile = io.open(filename, "w+")
+
+	http.request { 
+    url = url, 
+    sink = ltn12.sink.file(outputfile)
+	}
+
+	--should be triggered after the http request, 
+	--io.close(outputfile)
+
+end
+
+function FeedGetter:parseFeeds(url, filename)
+    local f = io.open(filename, "rb")
+    local rss = f:read("*all")
+    f:close()
+
+	local parsed, err = feedparser.parse(rss, url)
+
+	return parsed
+
+end
+
+function FeedGetter:getFeeds(url)
+	local feedFile = "feeds/feed.xml"
+
+	print (feedFile)
+	self.downloadFeeds(url, feedFile)
+
+	local feeds = self.parseFeeds(url, feedFile)
+
+	return feeds
+end
