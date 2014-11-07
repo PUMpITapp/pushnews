bd52c0e6aab0e6d2f37de748a1833fc2ee77ccb1
Pierre Leca <piele739@student.liu.se>
2014-11-07 10:48:42 +0100

    Removed depandancies to extra libraries, sample news feed are retrieved at the moment

--- src/feeds/feedgetter.lua
+++ src/feeds/feedgetter.lua
@@ -1,7 +1,6 @@
 local http = require("socket.http")
 local io = require("io")
 local ltn12 = require("ltn12")
-require "feedparser"
 require "feeds.feed"
 
 FeedGetter = {}
@@ -35,7 +34,16 @@ function FeedGetter:parseFeeds(url, filename)
   local rss = f:read("*all")
   f:close()
 
-	local parsed, err = feedparser.parse(rss, url)
+	local parsed = {
+		entries = {
+			{title = "War: What is it good for?", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 07:23:34 EDT", id = "http://edition.cnn.com/2014/10/10/business/war-economy/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ds3zG0DN0t4", source = "CNN"},
+			{title = "War: What is it good for?", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 07:23:34 EDT", id = "http://edition.cnn.com/2014/10/10/business/war-economy/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ds3zG0DN0t4", source = "CNN"},
+			{title = "War: What is it good for?", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 07:23:34 EDT", id = "http://edition.cnn.com/2014/10/10/business/war-economy/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ds3zG0DN0t4", source = "CNN"},
+			{title = "Keeping Ebola away from humans", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 06:52:22 EDT", id = "http://edition.cnn.com/2014/10/09/opinion/osofsky-ebola-wildlife/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ac87-SjauYU", source = "CNN"},
+			{title = "Keeping Ebola away from humans", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 06:52:22 EDT", id = "http://edition.cnn.com/2014/10/09/opinion/osofsky-ebola-wildlife/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ac87-SjauYU", source = "CNN"},
+			{title = "Keeping Ebola away from humans", summary = "The West is bombing terror group ISIS in Syria and Iraq, while Russia is engaged in a brittle stand-off in Eastern Ukraine. Bloodshed has dominated headlines through summer but, amid the misery, there is a winner: The defense industry.", updated = "Fri, 10 Oct 2014 06:52:22 EDT", id = "http://edition.cnn.com/2014/10/09/opinion/osofsky-ebola-wildlife/index.html", image = "http://feeds.feedburner.com/~r/rss/edition_europe/~4/Ac87-SjauYU", source = "CNN"},
+		}
+	}
 
 	return parsed
 end
@@ -45,7 +53,7 @@ function FeedGetter:translateResult(parsed, source)
 	local feeds = {}
 
 	for i, parsedFeed in ipairs(parsed.entries) do
- 		feeds[i] = Feed:new(parsedFeed.title, parsedFeed.summary, parsedFeed.updated, parsedFeed.id, source)
+ 		feeds[i] = Feed:new(parsedFeed.title, parsedFeed.summary, parsedFeed.updated, parsedFeed.id, source, parsedFeed.image)
  	end
 
  	return feeds
